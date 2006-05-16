ecb2a74752f3a41340386b5d36b64e4b05b866d4
Cristiano Basso <cristianobasso@gmail.com>
2006-05-16 19:38:39 +0000

    Template to Communty Edition Area

---
+++ templates/amteditcommunity.inc.php
@@ -0,0 +1,20 @@
+<?
+
+
+class AMTEditCommunity extends AMMain {
+  
+
+  function __construct() {
+    global $_CMAPP;
+    parent::__construct("azul2");
+
+    $this->setImgId($_CMAPP['imlang_url']."/top_editar_comunidade.gif");
+
+    $this->openNavMenu();
+
+  }
+}
+
+
+
+?>
