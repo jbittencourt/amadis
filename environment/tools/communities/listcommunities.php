52e3a5a04f8b845e22fa796764646e5f6b6334b1
Robson Mendonça <mwoc@gmail.com>
2007-06-22 21:28:45 +0000

    Bug fixes and close milestone 1.4

--- environment/tools/communities/listcommunities.php
+++ environment/tools/communities/listcommunities.php
@@ -10,10 +10,12 @@ $pag = new AMTCommunities;
 
 if(!isset($_REQUEST['list_action'])) $_REQUEST['list_action'] = "";
 
-if($_REQUEST['list_action'] == "A_list_news" || $_REQUEST['list_action'] == "A_list_projects"){
+$pag->add(new AMBCommunityList);
+
+/*if($_REQUEST['list_action'] == "A_list_news" || $_REQUEST['list_action'] == "A_list_projects"){
      $pag->add(new AMBCommunityList);
 }else{
   $pag->add(new AMBCommunitiesList);
 }
-     
+*/   
 echo $pag;
