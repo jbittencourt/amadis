830d1afe9f86647605b44a40db89739e0a5833af
Robson Mendonça <mwoc@gmail.com>
2007-07-16 16:46:38 +0000

    Soluction of the ticket #62. Now the register script don't accepts white spaces and uppercase strings.

--- environment/tools/webfolio/register.php
+++ environment/tools/webfolio/register.php
@@ -81,7 +81,7 @@ switch($_REQUEST['action']) {
       
  case "pag_1":
    //checa os dados do usuario, pra ver se nao existe o usuario
-   $_REQUEST['frm_username'] = strtolower($_REQUEST['frm_username']);
+   $_REQUEST['frm_username'] = ereg_replace('\ +','', trim(strtolower($_REQUEST['frm_username'])));
    $user = new AMUser;
    $user->username = $_REQUEST['frm_username'];
    try {
@@ -188,8 +188,8 @@ switch($_REQUEST['action']) {
    $cadBox->add("<input type=hidden name=action value=pag_2>");
    $cadBox->add("<p align=center>".$_language['frm_foto']);
    $cadBox->add("&nbsp;".$_language['valid_image_types']." ".implode(", ",$types).".");
-   $cadBox->add("<br><input type=file name=frm_foto onChange=\"this.form.submit()\">");
-   $cadBox->add("<br><input type=submit onClick=\"this.form['action'].value='pag_3'\" value=\"$_language[conclude]\">");
+   $cadBox->add("<br /><input type=file name=frm_foto onChange=\"this.form.submit()\">");
+   $cadBox->add("<br /><input type=submit onClick=\"this.form['action'].value='pag_3'\" value=\"$_language[conclude]\">");
    $cadBox->add("</form>");
 
    $cadBox->setTitle($_language['pag_2']);
