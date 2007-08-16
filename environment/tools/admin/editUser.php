68246f911fac9f755248d484f4658011fa494b38
Robson Mendonça <mwoc@gmail.com>
2007-08-16 18:30:36 +0000

    An exception not implented.

--- environment/tools/admin/editUser.php
+++ environment/tools/admin/editUser.php
@@ -17,44 +17,41 @@ $pag->add($box);
 $letra = $_REQUEST['initial'];
 
 if($letra != ""){
-  $q = new CMQuery("AMUser");
-  if(strlen($letra) == 1){ 
-    $q->setFilter("name LIKE '$letra%'");
-  }
-  $res = $q->execute();
+	$q = new CMQuery("AMUser");
+  	if(strlen($letra) == 1){ 
+    	$q->setFilter("name LIKE '$letra%'");
+  	}
+  	$res = $q->execute();
   
-  $conteudo .= "<table>";
-  if($res->__hasItems()){
-    foreach($res as $item){
-      $conteudo .= "<tr>"; 
-      $conteudo .= "<td width = '80'>";
+  	$conteudo .= "<table>";
+  	if($res->__hasItems()){
+    	foreach($res as $item){
+      		$conteudo .= "<tr>"; 
+      		$conteudo .= "<td width = '80'>";
 
-      $pag->add($conteudo);
-      $thumb = new AMUserThumb;
-      $thumb->codeFile = (integer) $item->picture;
-      try {
-	$thumb->load();
-	$pag->add($thumb->getView());
-      }
-      catch(CMDBException $e) {
-	echo $e; die();
-      }
+			$pag->add($conteudo);
+      		$thumb = new AMUserThumb;
+      		$thumb->codeFile = (integer) $item->picture;
+      		try {
+				$thumb->load();
+				$pag->add($thumb->getView());
+      		} catch(CMDBException $e) {
+				new AMLog('editUser.php thumb->load', $e, AMLog::LOG_CORE);
+      		}
 
-      $pag->add("</td><td>");
-      $pag->add(new AMTUserInfo($item));
-      $conteudo = "</td>";
-      $conteudo .= "<td width='90'>".AMMain::getChangePwButton($item->codeUser);
-      $conteudo .= "<td width='90'>".AMMain::getChangeStatusButton($item->codeUser);
-      $conteudo .= "<td width='90'>".AMMain::getSendNotificationButton($item->codeUser);
-      $conteudo .= "</tr>";    
-    }
-  }
-  else{
-    $conteudo .=  $_language['no_users_with_initial'];
-  }
+      		$pag->add("</td><td>");
+      		$pag->add(new AMTUserInfo($item));
+      		$conteudo = "</td>";
+      		$conteudo .= "<td width='90'>".AMMain::getChangePwButton($item->codeUser);
+      		$conteudo .= "<td width='90'>".AMMain::getChangeStatusButton($item->codeUser);
+      		$conteudo .= "<td width='90'>".AMMain::getSendNotificationButton($item->codeUser);
+      		$conteudo .= "</tr>";    
+    	}
+  	} else {
+    	$conteudo .=  $_language['no_users_with_initial'];
+  	}
 }
 $conteudo .= "</tr></table>";
 $pag->add($conteudo);
-
 
 echo $pag;
