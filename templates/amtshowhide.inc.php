3e9d2db45fa1aa7cce868d128e5dd0a6eda4bdde
Juliano Bittencourt <juliano.bittencourt@gmail.com>
2016-09-21 15:46:10 -0300

    Removing parameter from __toString to avoid bug

--- templates/amtshowhide.inc.php
+++ templates/amtshowhide.inc.php
@@ -1,17 +1,18 @@
-<?
+<?php
 /**
  * @package AMADIS
  * @subpackage AMTemplates
- */
+ **/
 
-class AMTShowHide extends CMHTMLObj { 
-  
+class AMTShowHide extends CMHTMLObj {
+
   const SHOW = "true";
   const HIDE = "false";
 
   private $initState, $script, $divName;
   private $lines = array();
   private $class='texto';
+  public $stringReturn=false;
 
   public function __construct($divName, $linkLabel, $displayState) {
 
@@ -41,18 +42,19 @@ class AMTShowHide extends CMHTMLObj {
     return $this->__toString(true);
   }
 
-  public function __toString($stringReturn=false) {
-    
+  public function __toString() {
+
     $link = "<a class=\"$this->class cursor\" onClick=\"AM_togleDivDisplay('$this->divName')\">".$this->linkLabel."</a><br />";
 
     $div[] = "<div id=\"$this->divName\" style=\"$this->initState\">";
-    
+
     $div[] = $this->lines;
 
     $div[] = "</div>";
-    
-    if($stringReturn) return array("link"=>$link, "box"=>$div);
-    else {
+
+    if($stringReturn) {
+      return array("link"=>$link, "box"=>$div);
+    } else {
       parent::add($link);
       parent::add($div);
       return parent::__toString();
