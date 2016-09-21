9a6ecb214c8214532c3ce4218893ca47261a55e4
Juliano Bittencourt <juliano.bittencourt@gmail.com>
2016-09-21 15:44:46 -0300

    Removing calendar browser due to errors in the Calendar library

--- templates/amboxblog.inc.php
+++ templates/amboxblog.inc.php
@@ -11,7 +11,7 @@
  * @author Juliano Bittencourt <juliano@lec.ufrgs.br>
  */
 class AMBoxBlog extends AMColorBox {
-    
+
     private $imagem;
     private $titulo;
     private $texto;
@@ -24,13 +24,13 @@ class AMBoxBlog extends AMColorBox {
     protected $month;
     protected $year;
 
-    public function __construct(CMContainer $list,$user,$titulo,$imagem=0,$texto="") 
+    public function __construct(CMContainer $list,$user,$titulo,$imagem=0,$texto="")
     {
         parent::__construct('', self::COLOR_BOX_PURPLE);
 
         $this->posts = $list;
         $this->user = $user;
-		
+
         $this->imagem = $imagem;
         $this->texto = $texto;
         $this->titulo = $titulo;
@@ -38,55 +38,57 @@ class AMBoxBlog extends AMColorBox {
         $this->requires("blog.js");
     }
 
-    public function addCabecalho($item) 
+    public function addCabecalho($item)
     {
         $this->cabecalho[] = $item;
     }
 
-    public function setDate($m,$y) 
+    public function setDate($m,$y)
     {
         $this->month =$m;
         $this->year = $y;
     }
 
-    public static function getPermanentLink($post) 
+    public static function getPermanentLink($post)
     {
         global $_CMAPP;
         return "$_CMAPP[services_url]/blog/blog.php?frm_codePost=$post->codePost#anchor_post_$post->codePost";
         //return "$_CMAPP[services_url]/blog/blog.php?frm_codePost=$post->codePost";
     }
-    
-    public function __toString() 
+
+    public function __toString()
     {
         global $_CMAPP,$_language;
 
         $url = $_CMAPP['images_url'];
-	
+
         //$js = "blog_handler_url = '$_CMAPP[services_url]/diario/comentarios.php';";
-        $js.= "blog_delete_link = 'blog.php?frm_action=A_delete&frm_codePost=';";
+        $js = "blog_delete_link = 'blog.php?frm_action=A_delete&frm_codePost=';";
         $js.= "blog_delete_message = '$_language[post_delete]';";
         $js.= "Blog_preLoadImages('$_CMAPP[images_url]/ico_seta_on_cmnt.gif','$_CMAPP[images_url]/ico_seta_off_cmnt.gif');";
 
         parent::addScript($js);
-		
-        
+
+
         $box = new AMColorBox('', AMColorBox::COLOR_BOX_INNERBLOG);
         $box->add("<div id='diary-header-picture'>");
         $box->add($this->imagem);
         $box->add("</div>");
-		
+
+
         /* CALENDAR */
-        $calendar = new AMTCalendar($this->month,$this->year);
-        if(!empty($_REQUEST['frm_codeUser']))
-        $calendar->setMoveLink("$_CMAPP[services_url]/blog/blog.php?frm_codeUser=$this->user&");
-        else
-        $calendar->setMoveLink("$_CMAPP[services_url]/blog/blog.php?");
-        
-        $box->add('<div style="margin-left: 5px;float:right;">'.$calendar->__toString().'</div>');
+        // $calendar = new AMTCalendar($this->month,$this->year);
+        //
+        // if(!empty($_REQUEST['frm_codeUser']))
+        //   $calendar->setMoveLink("$_CMAPP[services_url]/blog/blog.php?frm_codeUser=$this->user&");
+        // else
+        //   $calendar->setMoveLink("$_CMAPP[services_url]/blog/blog.php?");
+
+        //$box->add('<div style="margin-left: 5px;float:right;">'.$calendar->__toString().'</div>');
         /* END CALENDAR */
-        
+
         $box->add('<span class="diary-title">'.$this->titulo.'</span>');
-		
+
         $box->add("<acronym title='Really Simple Syndication' style='border: 0px;'>");
         $box->add("<a href='$this->rsslink'><img src='$_CMAPP[images_url]/rss_feed.gif' style='padding-left: 15px;'></a></acronym><br />");
 
@@ -95,23 +97,25 @@ class AMBoxBlog extends AMColorBox {
         $box->add("</div>");
 
         $box->add('<div id="diary-header"><br />');
-        
+
         if(empty($_REQUEST['frm_codeUser']) || $_REQUEST['frm_codeUser'] == $_SESSION['user']->codeUser) {
             $box->add(implode("",$this->cabecalho));
         }
         $box->add('</div>'); //diary header;
-        
+
         parent::add($box);
-        
-		unset($box);
-		
-		parent::add('<a name="editor"></a><div id="new-post" style="display:none;">');
-		
+        parent::add('<a name="editor"></a><div id="new-post" style="display:none;">');
+
+		    unset($box);
+
 		$requiredFields = array("title","body");
+
 		$form = new AMWSmartForm('AMBlogPost', "cad_post", $_SERVER['PHP_SELF'], $requiredFields, array('codePost'));
+
 		if(!empty($editar)) {
 			$form->loadDataFromObject($editar);
 		}
+
 
 		$form->submit_label = $_language['Publicar'];
 		$form->cancel_button->setOnClick("Blog.cancelEdit();");
@@ -121,29 +125,27 @@ class AMBoxBlog extends AMColorBox {
 		$form->setLabelClass("titpost");
 		$form->setRichTextArea("body");
 		$form->setDesign(CMWFormEl::WFORMEL_DESIGN_OVER);   // muda as labels do smart form
-		
+
 		$postForm = new AMColorBox('', AMColorBox::COLOR_BOX_INNERBLOG);
 		$postForm->add($form);
 		parent::add($postForm);
-		
+
 		parent::add('</div>');
-		
+
        /*
         *posts do diario
         */
-		
+
         if($this->posts->__hasItems()) {
 			$i=0;
             foreach($this->posts as $post) {
                 $impar = $i % 2;
                 $i++;
 
-                $calendar->pointDay(date('d',$post->time), "#anchor_post_$post->codePost");
-				
-                
-                
+                //$calendar->pointDay(date('d',$post->time), "#anchor_post_$post->codePost");
+
                 if(!$impar) {
-                	$box = new AMColorBox('', AMColorBox::COLOR_BOX_PURPLE);	
+                	$box = new AMColorBox('', AMColorBox::COLOR_BOX_PURPLE);
                 } else $box = new AMColorBox('', AMColorBox::COLOR_BOX_INNERBLOG);
 
                 $box->add('<h2 id="post-title-'.$post->codePost.'" class="titpost">'.$post->title.'</h2><span class="datapost"> - '.date("h:i ".$_language['date_format'],$post->time));
@@ -170,7 +172,7 @@ class AMBoxBlog extends AMColorBox {
                     $box->add("$l $_language[comments]<span style='font-size: 130%; color: red;'>($post->numComments)</span> <img id='post_comments_$post->codePost' src='$_CMAPP[images_url]/ico_seta_off_cmnt.gif'></a>");
                     $box->add("</span>");
                 }
-				
+
                 $tempo_post = time() - $post->time;
                 //if ($tempo_post < 86400){
                 if($tempo_post < 400000) {
@@ -182,19 +184,19 @@ class AMBoxBlog extends AMColorBox {
                         $box->add('&nbsp;&nbsp;&nbsp;<a href="#" onclick="Blog_deletePost('.$post->codePost.')" class="diary_edit"><img src="'.$_CMAPP['imlang_url'].'/icon_excluir.gif" border="0" align="baseline"></a></span>');
                     }
                 }
-                
+
                 $box->add("<div id='post_$post->codePost' style='display: none;'></div>");
                	parent::add($box);
             }
-            
+
         } else {
-        	
+
             parent::add("<br /><img src='$url/diario_markescuro.gif' alt='' />" );
             parent::add("<span class='datapost'>$_language[blog_empty]</span>");
         }
-        
+
         return parent::__toString();
-        
+
    }
 
 }
